7c4a1e9d38c5d7056623ba67278df11c6184b7a0
Lo1gr <louis.gruand@free.fr>
2019-02-20 09:48:01 +0100

    Update mapper1_test.py

--- mapper1_test.py
+++ mapper1_test.py
@@ -3,12 +3,12 @@
 import sys
 import os
 
-stopwords= ['a','able','about','across','after','all','almost','also','am','among','an','and','any','are','as','at','be','because','been','but','by',
-            'can','cannot','could','dear','did','do','does','either','else','ever','every','for','from','get','got','had','has','have','he','her','hers',
-            'him','his','how','however','i','if','in','into','is','it','its','just','least','let','like','likely','may','me','might','most','must','my',
-            'neither','no','nor','not','of','off','often','on','only','or','other','our','own','rather','said','say','says','she','should','since','so',
-            'some','than','that','the','their','them','then','there','these','they','this','tis','to','too','twas','us','wants','was','we','were','what',
-            'when','where','which','while','who','whom','why','will','with','would','yet','you','your'];
+# stopwords= ['a','able','about','across','after','all','almost','also','am','among','an','and','any','are','as','at','be','because','been','but','by',
+#             'can','cannot','could','dear','did','do','does','either','else','ever','every','for','from','get','got','had','has','have','he','her','hers',
+#             'him','his','how','however','i','if','in','into','is','it','its','just','least','let','like','likely','may','me','might','most','must','my',
+#             'neither','no','nor','not','of','off','often','on','only','or','other','our','own','rather','said','say','says','she','should','since','so',
+#             'some','than','that','the','their','them','then','there','these','they','this','tis','to','too','twas','us','wants','was','we','were','what',
+#             'when','where','which','while','who','whom','why','will','with','would','yet','you','your'];
 
 # input comes from STDIN (standard input)
 for line in sys.stdin:
@@ -17,6 +17,11 @@ for line in sys.stdin:
     line = line.strip()
     # split the line into words
     words = line.split()
+
+    stopwords=[]
+    for row in open('stopwords_en.txt','r'):
+        stopwords.append(row.strip())
+
     # increase counters
     for word in words:
         word=word.lower();
