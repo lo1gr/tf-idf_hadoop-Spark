58a30a9fec8ab821c912cf044b7402d34b916cd8
Lo1gr <louis.gruand@free.fr>
2019-03-01 18:08:50 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python
-from pyspark import SparkConf, SparkContext
-conf = (SparkConf()
-         .setMaster("local")
-         .setAppName("My app")
-         .set("spark.executor.memory", "1g"))
-sc = SparkContext(conf = conf)
+from pyspark import SparkContext
+sc = SparkContext("local", "first app")
 
 text = sc.textFile("hdfs:///user/hadoop/tfidf/input/text_0.txt")
 text.count()
+
+
+# from pyspark import SparkContext
+# logFile = "file:///home/hadoop/spark-2.1.0-bin-hadoop2.7/README.md"
+# sc = SparkContext("local", "first app")
+# logData = sc.textFile(logFile).cache()
+# numAs = logData.filter(lambda s: 'a' in s).count()
+# numBs = logData.filter(lambda s: 'b' in s).count()
+# print "Lines with a: %i, lines with b: %i" % (numAs, numBs)
