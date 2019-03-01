43e20e8233eebe148dfd8188ee9c6795edbeb034
Lo1gr <louis.gruand@free.fr>
2019-03-01 18:02:49 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python
+from pyspark import SparkConf, SparkContext
+conf = (SparkConf()
+         .setMaster("local")
+         .setAppName("My app")
+         .set("spark.executor.memory", "1g"))
+sc = SparkContext(conf = conf)
 
 text = sc.textFile("hdfs:///user/hadoop/tfidf/input/text_0.txt")
 text.count()
